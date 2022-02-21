2d30114fdbf8c0300b1cd609b24285b3de98dbf2
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-21 16:21:21 -0500

    rendering dayListItems to dayList

---
+++ src/components/DayList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+import DayListItem from "./DayListItem";
+
+// import "components/DayList.scss";
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+
+  const constructedDays = days.map((curr) => (
+    <DayListItem
+      key={curr.id}
+      name={curr.name}
+      spots={curr.spots}
+      selected={curr.name === day}
+      setDay={setDay}
+    ></DayListItem>
+  ));
+
+  console.log(constructedDays, days);
+
+  // const classes = classNames();
+
+  return <ul>{constructedDays}</ul>;
+}

2f3f6ccfcc52a1f10aa9732c0d0a604f56d09b91
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-21 15:26:41 -0500

    updating button component to take in, and use, props

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,16 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let classes = "button";
+  const { confirm, danger, disabled } = props;
+
+  if (confirm) classes += " button--confirm";
+  if (danger) classes += " button--danger";
+  if (disabled) classes += " button--disabled";
+
+  return (
+    <button onClick={props.onClick} className={classes} disabled={disabled}>
+      {props.children}
+    </button>
+  );
 }

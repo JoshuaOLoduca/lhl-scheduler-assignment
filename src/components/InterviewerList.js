f4efa0b7b33a9ac708f62a05689fe36ea6fe996e
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-22 12:02:45 -0500

    initial setup of InterViewerList Component w/ storybook addition

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import InterViewerListItem from "./InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterViewerList(props) {
+  const { interviewers, interviewer } = props;
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}

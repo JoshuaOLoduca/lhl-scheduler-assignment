dee91f0beeb670aec3cc0630ba05249cfc438112
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-22 11:58:03 -0500

    adding styling to Interviewer list item based on props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,11 +4,16 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterViewerListItem(props) {
-  const { id, name, avatar, setInterviewer } = props;
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const classes = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
+
   return (
-    <li className="interviewers__item" onClick={() => setInterviewer(id)}>
+    <li className={classes} onClick={() => setInterviewer(id)}>
       <img className="interviewers__item-image" src={avatar} alt={name} />
-      Sylvia Palmer
+      {selected && name}
     </li>
   );
 }

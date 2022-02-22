dd6fc7b4daade95975777b3960b850ade863b673
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-22 12:16:03 -0500

    updating interviewerList to pass props to interviewerlistItem and to render them to component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,12 +4,22 @@ import InterViewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterViewerList(props) {
-  const { interviewers, interviewer } = props;
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const intervListItems = interviewers.map((elm) => (
+    <InterViewerListItem
+      name={elm.name}
+      id={elm.id}
+      avatar={elm.avatar}
+      selected={interviewer === elm.id}
+      setInterviewer={setInterviewer}
+    />
+  ));
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{intervListItems}</ul>
     </section>
   );
 }

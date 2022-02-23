c2f6d293097f5ff7562c936c8cd7fbf33c992a11
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-22 19:46:30 -0500

    adding function to get appointments for specific day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,16 @@
+import Appointment from "components/Appointment";
+
 export function getAppointmentsForDay(state, day) {
   //... returns an array of appointments for that day
+  const appointmentsForDay = [];
+
+  state.days.forEach((item) => {
+    if (item.name === day) {
+      item.appointments.forEach((id) =>
+        appointmentsForDay.push(state.appointments[id])
+      );
+    }
+  });
+
+  return appointmentsForDay;
 }

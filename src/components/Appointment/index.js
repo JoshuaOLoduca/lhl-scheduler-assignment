0919f550b385c9de1b685716ee4e9249a1a28cf3
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-23 13:19:56 -0500

    Adding view state to appointment component to support adding interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,22 +2,33 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form.js";
 
 import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? (
-        <Show student={interview.student} interviewer={interview.interviewer} />
-      ) : (
-        <Empty />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
       )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   );
 }

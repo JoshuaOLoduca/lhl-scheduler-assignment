b69bf1d028e50891310b7c931512bbeda8ef0bea
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-28 14:28:21 -0500

    adding delete and delete confirmation for scheduled interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,17 +4,19 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form.js";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 import useVisualMode from "hooks/useVisualMode";
 
-import {getInterviewersForDay} from "../../helpers/selectors"
-
 import "./styles.scss";
+import axios from "axios";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETE = "DELETE";
+const DELETING = "DELETING";
 
 export default function Appointment(props) {
   const { time, interview, bookInterview, id, interviewers} = props;
@@ -35,15 +37,41 @@ export default function Appointment(props) {
       .then(() => transition(SHOW))
   }
 
+
+  function deleteInterview(appointmentId) {
+    transition(DELETING);
+
+    axios.delete(`/api/appointments/${appointmentId}`)
+      .then(() => transition(EMPTY))
+      .catch(e => console.log(e))
+
+  }
+
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
       {mode === SHOW && (
-        <Show student={interview.student} interviewer={interview.interviewer} />
+        <Show
+          student={ interview.student }
+          interviewer={ interview.interviewer }
+          onDelete={ () => transition(DELETE) } />
       )}
-      {mode === CREATE && <Form interviewers={[...interviewers]} onCancel={() => back()} onSave={save} />}
-      {mode === SAVING && <Status message={'Saving'} />}
+
+      { mode === CREATE && <Form
+        interviewers={ [...interviewers] }
+        onCancel={ back }
+        onSave={ save } /> }
+
+      { mode === SAVING && <Status message={ 'Saving' } /> }
+
+      { mode === DELETING && <Status message={ 'Deleting' } /> }
+      
+      { mode === DELETE && <Confirm
+        message={ `Delete Interview for ${interview.student}?` }
+        onConfirm={ () => deleteInterview(id) }
+        onCancel={ back } /> }
     </article>
   );
 }

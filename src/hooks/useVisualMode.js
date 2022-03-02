d473b072383e798dc7cfe068bfb6f4938160812b
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-02 18:09:05 -0500

    useVisualMode reducer

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
+// View manager
+// Use transition to change view
+// Back to go back to most previous view
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([]);
 
+  // Sets mode to provided one
+  // Will replace latest history elm if replace is set to true
   function transition(view, replace = false) {
     setHistory((prev) => (replace ? [...prev] : [...prev, mode]));
     setMode(view);
   }
 
+  // Sets mode to latest history elm
   function back() {
     if (history.length === 0) return;
     setMode(history.pop());

9b3150d42f2a3ada38439c66408f432cff367b83
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-02 19:24:14 -0500

    commenting/cleaning and bugfixing appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Form from "./Form.js";
 import Status from "./Status";
 import Confirm from "./Confirm";
 import Error from "./Error";
+import PropTypes from "prop-types";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -31,38 +32,53 @@ export default function Appointment(props) {
     interviewers,
     deleteUpdate,
   } = props;
+
+  // Initializing view manager
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  // Saves interview to database/state
   function save(name, interviewer) {
     const newInterview = {
       student: name,
       interviewer,
     };
 
+    // Set view mode to saving status
     transition(SAVING);
 
+    // Save interview in db and state
     bookInterview(id, newInterview)
+      // When done, show interview
       .then(() => transition(SHOW))
+      // If it fails, let user know
       .catch((e) => transition(ERROR_SAVING, true));
   }
 
+  // For deleting an interview
   function cancelInterview(appointmentId) {
+    // Show deleting status
     transition(DELETING);
 
+    // Delete interview from db and state
     deleteInterview(appointmentId)
+      // When done, show add button
       .then(() => transition(EMPTY))
+      // if it cant, let user know
       .catch((e) => transition(ERROR_DELETE, true));
   }
 
+  // Preventing stale states
+  // Runs on interview update, transition call or on view change (last 2 are basically the same)
   useEffect(() => {
-    if (interview && (mode === EMPTY || mode === CREATE)) {
-      transition(SHOW);
-    }
-    if (interview === null && mode === SHOW) {
+    // If the interview is null and we are editing or showing an interview, stop that
+    if (interview === null && (mode === SHOW || mode === EDIT)) {
       transition(EMPTY);
     }
+
+    // If we have an interview with the interview attribute,
+    // Show the interview, no matter what the user is doing to it
     if (interview && interview.update) {
       deleteUpdate();
       transition(SHOW);
@@ -72,8 +88,11 @@ export default function Appointment(props) {
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={time} />
+
+      {/* If view mode is Empty, show add button */}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
 
+      {/* If mode is show and we have an interview, show scheduled interview */}
       {mode === SHOW && interview && (
         <Show
           student={interview.student}
@@ -83,14 +102,19 @@ export default function Appointment(props) {
         />
       )}
 
+      {/* If we clicked add from empty state */}
+      {/* Show create form */}
       {mode === CREATE && (
         <Form interviewers={[...interviewers]} onCancel={back} onSave={save} />
       )}
 
+      {/* Once we confirm an interview, show user saving circle */}
       {mode === SAVING && <Status message={"Saving"} />}
 
+      {/* Show deleting circle if user confirmed delete */}
       {mode === DELETING && <Status message={"Deleting"} />}
 
+      {/* Asking for delete confirmation */}
       {mode === DELETE && (
         <Confirm
           message={`Delete Interview for ${interview.student}?`}
@@ -99,23 +123,36 @@ export default function Appointment(props) {
         />
       )}
 
+      {/* Showing edit form */}
       {mode === EDIT && (
         <Form
-          student={interview.student}
+          student={interview && interview.student}
           interviewers={[...interviewers]}
-          interviewer={interview.interviewer.id}
+          interviewer={interview && interview.interviewer.id}
           onCancel={back}
           onSave={save}
         />
       )}
 
+      {/* Show error on db saving failure */}
       {mode === ERROR_SAVING && (
         <Error message={"Couldnt save details"} onClose={back} />
       )}
 
+      {/* Show error on db delete failure */}
       {mode === ERROR_DELETE && (
         <Error message={"Couldnt delete appointment"} onClose={back} />
       )}
     </article>
   );
 }
+
+Appointment.propTypes = {
+  time: PropTypes.string,
+  interview: PropTypes.object,
+  bookInterview: PropTypes.func,
+  deleteInterview: PropTypes.func,
+  id: PropTypes.number,
+  interviewers: PropTypes.array,
+  deleteUpdate: PropTypes.func,
+};

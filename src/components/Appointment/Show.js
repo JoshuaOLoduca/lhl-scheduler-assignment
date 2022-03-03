61d3f6949c6d3b0f0a116cb980a756981fccaeea
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-02 19:03:13 -0500

    commenting/cleaning show and status componenets

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./styles.scss";
 
@@ -8,20 +9,28 @@ export default function Show(props) {
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">{student}</h2>
+        <h2 className="text--regular">
+          {/* Students Name */}
+          {student}
+        </h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewer.name}</h3>
+          <h3 className="text--regular">
+            {/* Interviewers Name */}
+            {interviewer.name}
+          </h3>
         </section>
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
+          {/* Edit Button */}
           <img
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
             onClick={onEdit}
           />
+          {/* Delete Button */}
           <img
             className="appointment__actions-button"
             src="images/trash.png"
@@ -33,3 +42,10 @@ export default function Show(props) {
     </main>
   );
 }
+
+Show.propTypes = {
+  student: PropTypes.string.isRequired,
+  interviewer: PropTypes.object.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
